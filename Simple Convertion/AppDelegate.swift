f35a96b231cdfdbf1f62bcf46c49b37b8903989a
Tim Constantinov <timurko@timurko.local>
2018-12-14 21:58:08 +0700

    Initial Commit

---
+++ Simple Convertion/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Simple Convertion
+//
+//  Created by Tim Constantinov on 14.12.18.
+//  Copyright © 2018 TiM. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Simple Convertion/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Simple Convertion
+//
+//  Created by Tim Constantinov on 14.12.18.
+//  Copyright © 2018 TiM. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
